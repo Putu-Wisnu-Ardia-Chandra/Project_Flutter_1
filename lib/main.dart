4918fde5c808f87e68c86ef827e22317baecd55d
wisnu <wisnu.ardia@undiksha.ac.id>
2021-03-04 07:28:13 +0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
@@ -9,8 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: Text('Aplikasi Harem Squad'),
+          backgroundColor: Colors.blueAccent,
+          title: Text('Profil'),
+          centerTitle: true,
           leading: Icon(
             Icons.dashboard,
           ),
@@ -27,21 +30,194 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Column(
-            children: [
-              Image(
-                image: NetworkImage('https://mahasiswa.undiksha.ac.id/media/photo/p_1915051017.jpg'),
-              ),
-              Padding(
-                padding: EdgeInsets.all(18.0),
-                child: Text(
-                  'CEO : Putu Wisnu Ardia Chandra',
-                  style: TextStyle(fontSize: 22),
-                ),
-              ),
+            children: <Widget>[
+              Pictures(),
+              TextName(),
+              FirstRow(),
+              SecondRow(),
             ],
           ),
         ),
       ),
     );
-  } 
+  }
+}
+
+class Pictures extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      margin: const EdgeInsets.only(top: 40.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.100),
+        image: DecorationImage(
+          image: NetworkImage('https://pbs.twimg.com/profile_images/1288311151797788672/JGaoR4bG_400x400.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class TextName extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "Putu Wisnu Ardia Chandra",
+        style: TextStyle(
+          fontSize: 24,
+          color: Colors.black,
+        ),
+      ),
+      margin: const EdgeInsets.only(top: 20.0),
+    );
+  }
+}
+
+class FirstRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10.0,
+              ),
+            ],
+          ),
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.school,
+                    size: 50,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    'Undiksha',
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.location_city,
+                    size: 50,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    'Amlapura',
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SecondRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.music_note,
+                    size: 50,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    'DJ',
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          width: 150,
+          margin: const EdgeInsets.only(top: 20.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.favorite,
+                    size: 50,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    'Chess',
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
